68cee431ad5ceac3b676f068400f4fb59a741b3d
hironori-hashimoto <h.hashimoto@mjeinc.co.jp>
2021-03-29 15:11:48 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app id="inspire">
+    <v-card width="400px" class="mx-auto mt-5">
+      <v-card-title class="font-weight-bold">
+        <h1 class="display-1">ログイン</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field prepend-icon="mdi-account-circle" label="ユーザ名" />
+          <v-text-field
+            prepend-icon="mdi-lock"
+            append-icon="mdi-eye-off"
+            type="password"
+            label="パスワード"
+          />
+          <v-card-actions>
+            <v-btn class="info">ログイン</v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo,
+  },
+};
+</script>
